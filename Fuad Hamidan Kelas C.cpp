20b5cab4034c907f06f8cc8cca39b08035673ac0
FuadHmdn <112995147+FuadHmdn@users.noreply.github.com>
2022-09-08 14:17:39 +0700

    Add files via upload

---
+++ Fuad Hamidan Kelas C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int A;
+	cout<<"Angka Pertama : ";
+	cin>>A;
+	
+	int B;
+	cout<<"Angka Kedua : ";
+	cin>>B;
+	
+	cout<< A + B <<endl;
+	cout<< A - B <<endl;
+	cout<< A * B <<endl;
+	cout<< A / B <<endl;
+	cout<< A % B;
+	
+	return 0;
+}
