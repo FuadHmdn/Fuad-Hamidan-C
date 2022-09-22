9b24f33450bc6be3045b99bad26d1dc349b1360b
FuadHmdn <112995147+FuadHmdn@users.noreply.github.com>
2022-09-22 12:51:25 +0700

    Add files via upload

---
+++ FuadHamidan_2257051025_praktest2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int angka;
+	cin>>angka;
+	
+	if (angka ==1 ){
+		cout<<"satu\n";
+	}
+	else if (angka ==2 ){
+		cout<<"dua\n";
+	}
+	else if (angka ==3 ){
+		cout<<"tiga\n";
+	}
+	else if (angka >3) {
+		cout<<"Bilangan Lebih Besar Dari Tiga\n";
+	}
+	
+	return 0;
+	
+}
