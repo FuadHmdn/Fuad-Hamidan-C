63c94d475b41e43c852696c5dda77e6ce456cce9
FuadHmdn <112995147+FuadHmdn@users.noreply.github.com>
2022-09-29 13:15:19 +0700

    Add files via upload

---
+++ FuadHamidan_2257051025_praktest3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int fuad, hmdn, a;
+	cin>>a;
+	cin>>hmdn;
+	for(fuad = 0;fuad <= hmdn; fuad+=a){
+		
+		cout<<fuad<<endl;
+	}
+	return 0;
+}
